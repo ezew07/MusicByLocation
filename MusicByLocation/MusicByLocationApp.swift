0b15a2e46222d09f125f8b9404b39b32e5fa2334
William Eze <eze.w@etoncollege.org.uk>
2024-03-12 16:53:35 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by William Eze on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
